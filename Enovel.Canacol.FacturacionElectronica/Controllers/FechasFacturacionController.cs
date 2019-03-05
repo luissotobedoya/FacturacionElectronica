e7f1eebcce4c82e40c35e90a1cd4eac52c58b8b8
Daniel <danielgiraldoch@gmail.com>
2019-03-05 08:38:35 -0500

    FechasFacturación Controlador y vista

--- Enovel.Canacol.FacturacionElectronica/Controllers/FechasFacturacionController.cs
+++ Enovel.Canacol.FacturacionElectronica/Controllers/FechasFacturacionController.cs
@@ -1,11 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Enovel.Canacol.FacturacionElectronica.Controllers
 {
     public class FechasFacturacionController : Controller
     {
-        [Authorize]
-        public ActionResult FechasFacturacion()
+        // GET: FechasFacturacion
+        public ActionResult Index()
         {
             return View();
         }

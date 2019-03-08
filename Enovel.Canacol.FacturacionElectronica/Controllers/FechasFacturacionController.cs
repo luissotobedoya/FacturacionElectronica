6a3ae5b305b6a4b20cd85c1c1ee84c54eb870300
Daniel <danielgiraldoch@gmail.com>
2019-03-08 11:40:49 -0500

    cambios daniel

---
+++ Enovel.Canacol.FacturacionElectronica/Controllers/FechasFacturacionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Enovel.Canacol.FacturacionElectronica.Controllers
+{
+    public class FechasFacturacionController : Controller
+    {
+        // GET: FechasFacturacion
+        public ActionResult GestionarFechas()
+        {
+            string PeriodoActual = DateTime.Now.Year.ToString();
+            ViewBag.PeriodoActual = PeriodoActual;
+            return View();
+        }
+    }
+}
